56b93b1de1b6dc1bf02714eddaf3d1e32cfb22e2
iPrathamKumkar <prathameshkumkar@gmail.com>
2020-03-22 02:38:21 -0700

    Renamed links to urls

--- crawler.py
+++ crawler.py
@@ -27,18 +27,18 @@ class Crawler:
         while self.to_crawl:
             self.executor.submit(self.crawl_task, self.to_crawl.get(timeout=self.max_timeout))
 
-    def crawl_task(self, url):
+    def crawl_task(self, url_to_crawl):
         try:
-            response = requests.get(url, timeout=self.max_timeout)
+            response = requests.get(url_to_crawl, timeout=self.max_timeout)
         except requests.exceptions.RequestException as e:
             print('Request failed: ' + str(e))
-        self.mark_page_visited(url)
+        self.mark_visited(url_to_crawl)
         if self.response_status_ok(response):
-            links = self.get_all_links_on_page(response)
-            self.print_urls(url, links)
-            self.add_links_to_crawl(links)
+            urls = self.get_all_urls_on_page(response)
+            self.print_urls(url_to_crawl, urls)
+            self.add_urls_to_crawl(urls)
 
-    def mark_page_visited(self, url):
+    def mark_visited(self, url):
         self.visited_lock.acquire()
         self.scraped_pages.add(url)
         self.visited_lock.release()
@@ -46,26 +46,26 @@ class Crawler:
     def response_status_ok(self, response):
         return response and response.status_code == self.STATUS_OK
 
-    def get_all_links_on_page(self, response):
+    def get_all_urls_on_page(self, response):
         soup = BeautifulSoup(response.content, self.HTML_PARSER)
         a_tags = soup.find_all('a', href=True)
-        links = list()
+        urls = list()
         for tag in a_tags:
             url = tag[self.HREF]
             if self.check_valid_url(url):
-                links.append(url)
-        return links
+                urls.append(url)
+        return urls
 
-    def add_links_to_crawl(self, links):
-        for link in links:
-            if link not in self.scraped_pages:
-                self.to_crawl.put(link)
+    def add_urls_to_crawl(self, urls):
+        for url in urls:
+            if url not in self.scraped_pages:
+                self.to_crawl.put(url)
 
-    def print_urls(self, url, links):
+    def print_urls(self, url, urls):
         self.print_lock.acquire()
         print(url)
-        for link in links:
-            print('\t' + str(link))
+        for url in urls:
+            print('\t' + str(url))
         self.print_lock.release()
 
     def set_timeout(self, timeout_in_sec):
@@ -76,12 +76,12 @@ if __name__ == "__main__":
     try:
         base_url = sys.argv[1].strip()
     except IndexError:
-        print('Enter a URL to start crawling.')
+        print('Enter a URL to start crawling\nRun the program as "python crawler.py <url>"')
         sys.exit(1)
     crawler = Crawler(4)
     if crawler.check_valid_url(base_url):
         crawler.to_crawl.put(base_url)
         crawler.crawl()
     else:
-        print('Enter a valid url.')
+        print('Enter a valid URL\nURL must begin with http:// or https://')
         sys.exit(1)
