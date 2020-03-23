4445b3d936f7c7d6b0f35798f9534c98790163e3
iPrathamKumkar <prathameshkumkar@gmail.com>
2020-03-22 20:39:07 -0700

    Code cleanup

--- crawler.py
+++ crawler.py
@@ -8,43 +8,36 @@ from bs4 import BeautifulSoup
 
 class Crawler:
     DEFAULT_MAX_TIMEOUT = 10
+    DEFAULT_MAX_WORKERS = 4
     STATUS_OK = 200
     HTML_PARSER = 'html.parser'
     HREF = 'href'
 
-    def __init__(self, num_workers):
+    def __init__(self):
         self.scraped_pages = set()
         self.to_crawl = Queue()
-        self.executor = ThreadPoolExecutor(max_workers=num_workers)
+        self.max_workers = self.DEFAULT_MAX_WORKERS
         self.max_timeout = self.DEFAULT_MAX_TIMEOUT
         self.print_lock = Lock()
         self.visited_lock = Lock()
 
-    def check_valid_url(self, url):
-        return url.startswith('http')
+    def set_max_workers(self, max_workers):
+        self.max_workers = max_workers
 
-    def crawl(self):
-        while self.to_crawl:
-            self.executor.submit(self.crawl_task, self.to_crawl.get(timeout=self.max_timeout))
+    def set_max_timeout(self, timeout_in_sec):
+        self.max_timeout = timeout_in_sec
 
-    def crawl_task(self, url_to_crawl):
-        try:
-            response = requests.get(url_to_crawl, timeout=self.max_timeout)
-        except requests.exceptions.RequestException as e:
-            print('Request failed: ' + str(e))
-        self.mark_visited(url_to_crawl)
-        if self.response_status_ok(response):
-            urls = self.get_all_urls_on_page(response)
-            self.print_urls(url_to_crawl, urls)
-            self.add_urls_to_crawl(urls)
+    def print_urls(self, url, urls):
+        self.print_lock.acquire()
+        print(url)
+        for url in urls:
+            print('\t' + str(url))
+        self.print_lock.release()
 
-    def mark_visited(self, url):
-        self.visited_lock.acquire()
-        self.scraped_pages.add(url)
-        self.visited_lock.release()
-
-    def response_status_ok(self, response):
-        return response and response.status_code == self.STATUS_OK
+    def add_urls_to_crawl(self, urls):
+        for url in urls:
+            if url not in self.scraped_pages:
+                self.to_crawl.put(url)
 
     def get_all_urls_on_page(self, response):
         soup = BeautifulSoup(response.content, self.HTML_PARSER)
@@ -56,32 +49,43 @@ class Crawler:
                 urls.append(url)
         return urls
 
-    def add_urls_to_crawl(self, urls):
-        for url in urls:
-            if url not in self.scraped_pages:
-                self.to_crawl.put(url)
+    def response_status_ok(self, response):
+        return response and response.status_code == self.STATUS_OK
 
-    def print_urls(self, url, urls):
-        self.print_lock.acquire()
-        print(url)
-        for url in urls:
-            print('\t' + str(url))
-        self.print_lock.release()
+    def mark_visited(self, url):
+        self.visited_lock.acquire()
+        self.scraped_pages.add(url)
+        self.visited_lock.release()
 
-    def set_timeout(self, timeout_in_sec):
-        self.max_timeout = timeout_in_sec
+    def crawl_task(self, url_to_crawl):
+        try:
+            response = requests.get(url_to_crawl, timeout=self.max_timeout)
+            if self.response_status_ok(response):
+                self.mark_visited(url_to_crawl)
+                urls = self.get_all_urls_on_page(response)
+                self.print_urls(url_to_crawl, urls)
+                self.add_urls_to_crawl(urls)
+        except requests.exceptions.RequestException as e:
+            print('Request failed: ' + str(e))
 
+    def run_crawler(self):
+        executor = ThreadPoolExecutor(max_workers=self.max_workers)
+        while self.to_crawl:
+            executor.submit(self.crawl_task, self.to_crawl.get(timeout=self.max_timeout))
+
+    def check_valid_url(self, url):
+        return url.startswith('http')
 
 if __name__ == "__main__":
     try:
         base_url = sys.argv[1].strip()
     except IndexError:
-        print('Enter a URL to start crawling\nRun the program as "python crawler.py <url>"')
+        print('Enter a URL to start crawling')
         sys.exit(1)
-    crawler = Crawler(4)
+    crawler = Crawler()
     if crawler.check_valid_url(base_url):
         crawler.to_crawl.put(base_url)
-        crawler.crawl()
+        crawler.run_crawler()
     else:
-        print('Enter a valid URL\nURL must begin with http:// or https://')
+        print('Enter a valid URL')
         sys.exit(1)
